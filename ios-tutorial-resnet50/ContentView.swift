dede4f2960845be2fe2bd54fc09d6825bc0f6f79
将之大西 <omccello@gmail.com>
2021-08-12 23:59:08 +0900

    add

--- ios-tutorial-resnet50/ContentView.swift
+++ ios-tutorial-resnet50/ContentView.swift
@@ -2,8 +2,19 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("柴犬です")
+                .padding()
+                .font(.title)
+            Image("shiba")
+                .resizable()
+                .frame(width: 300, height: 200)
+            Button(action: {
+            }, label: {
+                Text("この画像は何だろう？")
+                    .padding()
+            })
+        }
     }
 }
 
