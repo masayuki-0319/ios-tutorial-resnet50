5144a2425ea5c763a56dd45354b888f8cbfcc04a
将之大西 <omccello@gmail.com>
2021-08-12 23:50:15 +0900

    Initial Commit

---
+++ ios-tutorial-resnet50/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ios-tutorial-resnet50
+//
+//  Created by 大西将之 on 2021/08/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ios-tutorial-resnet50/ios_tutorial_resnet50App.swift
@@ -0,0 +1,17 @@
+//
+//  ios_tutorial_resnet50App.swift
+//  ios-tutorial-resnet50
+//
+//  Created by 大西将之 on 2021/08/12.
+//
+
+import SwiftUI
+
+@main
+struct ios_tutorial_resnet50App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
